fbae5cab9485094e5b82b7d50426b08a71ccb7b0
Emma Skillings <emmarskillings@gmail.com>
2019-09-21 13:57:30 -0400

    Added palette to project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import palette from '../theme/palette';
+import palette from '../theme/';
 
 export default () => {
   return (
---
+++ src/theme/palette.js
@@ -0,0 +1,15 @@
+const primary = '#EAE7DC';
+const secondary = '#F1DAC4';
+const tertiary = '#D7907B';
+const accent1 = '#5B6322';
+const accent2 = '#3A606E'
+const text = '#242728';
+
+export default {
+  primary,
+  secondary,
+  tertiary,
+  accent1,
+  accent2,
+  text, 
+}
